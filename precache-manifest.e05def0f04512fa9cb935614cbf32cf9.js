816fe4a2b6aaee592fae0a526f363600f7459273
Ivan Komjenic <komjenic@gmail.com>
2019-08-06 03:26:19 +0200

    Updates

---
+++ precache-manifest.e05def0f04512fa9cb935614cbf32cf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb118998bbc6f653245e7eddc1c46997",
+    "url": "/ivan-komjenic-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "60c4c76b474f8f875102",
+    "url": "/ivan-komjenic-monsters-rolodex/static/css/main.719f7ca2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ivan-komjenic-monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "60c4c76b474f8f875102",
+    "url": "/ivan-komjenic-monsters-rolodex/static/js/main.39a1c2ec.chunk.js"
+  },
+  {
+    "revision": "cb961aeb8101c625946d",
+    "url": "/ivan-komjenic-monsters-rolodex/static/js/runtime~main.34ec8d68.js"
+  }
+]);
